69c15e57fdeaadbf8bf4c021926b652ae8d68d7b
Muhammad Mubarak <muhammadmarch09@gmail.com>
2025-05-23 19:10:37 +0600

    Two sum problem is solved using the nested for loop comparison with target displaying the return value with the pseudocode code testing those use cases

--- Readme.md
+++ Readme.md
@@ -1,4 +1,6 @@
 # Problem Solving 
 ### In this repo I will try to solve javascript problems from begginer to advance level.
 ## With Pseudocode </>
+
+You will find Those pseudocode with solution file .
      
---
+++ two-sum.js
@@ -0,0 +1,30 @@
+// Given an array of integers nums and an integers target
+
+//  return indeces of two numbers such that they add up to target
+
+// PseudoCode :
+
+//   function (nums, target)
+
+//     for i from 0 to length of nums - 1
+
+//         for j from i + 1 to length of nums
+
+//             if num[i] + num[j] == target
+
+//                 return [i, j]
+
+// Solution
+
+function twoSum(nums, target) {
+  for (let i = 0; i <= nums.length; i++) {
+    for (let j = i + 1; j <= nums.length; i++) {
+      if (nums[i] + nums[j] == target) {
+        return [i, j];
+      }
+    }
+  }
+}
+
+
+console.log(twoSum([2,3,1,5,6],6))
